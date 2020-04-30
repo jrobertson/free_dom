0e25d5e5cc144425af698fcef013153eb3025f3f
James Robertson <james@jamesrobertson.eu>
2020-04-30 11:39:07 +0100

    feature: Implemented the onchange trigger.

--- free_dom.gemspec
+++ free_dom.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'free_dom'
-  s.version = '0.1.0'
+  s.version = '0.2.0'
   s.summary = 'Dynamically builds a DOM from XML.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/free_dom.rb']
--- lib/free_dom.rb
+++ lib/free_dom.rb
@@ -7,24 +7,56 @@ require 'domle'
 
 class FreeDom < Domle
 
-  def initialize(xml)
+  def initialize(xml, debug: false)
 
-    doc = Rexle.new xml
+    @doc, @debug = Rexle.new(xml), debug
 
     h = {}
 
-    doc.root.each_recursive do |e|
+    @doc.root.each_recursive do |e|
       h[e.name.to_sym] ||= {}
       h[e.name.to_sym].merge!(e.attributes)
     end
 
     @defined_elements = {}
 
-    h.each do |name, attributes|
+    h.each do |name, attributelist|
 
       klass = Class.new(Domle::Element) do
 
-        attr2_accessor *attributes.keys
+        a = attributelist.keys
+        
+        triggers = a.select {|x| x =~ /^trigger_/ }                
+        
+        attr2_accessor *(a - triggers)
+        
+        triggers.each do |x|
+
+          trigger = x.to_s[/(?<=^trigger_).*/].to_sym
+          puts 'trigger: ' + trigger.inspect if @debug
+          
+          define_method(trigger)  do
+            eval method(('on' + trigger.to_s).to_sym).call, $env
+          end
+
+          if trigger == :change then
+            
+            #puts 'change found'
+            
+            attribute = attributelist[x].to_sym
+            
+            define_method((attribute.to_s + '=').to_sym) do |val|
+              oldval = attributes[attribute]
+              attributes[attribute] = val
+              #puts 'inside change='
+              @rexle.refresh if @rexle
+              change() unless val == oldval
+              val
+            end
+            
+          end
+          
+        end        
 
       end
 
@@ -32,9 +64,31 @@ class FreeDom < Domle
       @defined_elements.merge!({name => custom_class})
 
     end
+    
+    # remove the trigger declaration attributes
+    @doc.root.each_recursive do |e|
+      e.attributes.keys.select {|x| x =~ /^trigger_/ }\
+          .each {|x| e.attributes.delete x }
+    end
 
-    super(xml)
+    super(@doc.root.xml)        
+    script()
+                                     
   end
+  
+  
+  # used within the scope of the script tags
+  #
+  def doc()
+    self
+  end
+  
+  def script()
+    s = @doc.root.xpath('//script').map {|x| x.text.unescape }.join
+    eval s
+    $env = binding
+  end                                      
+                                    
 
   private
   
@@ -43,5 +97,3 @@ class FreeDom < Domle
   end
 
 end
-
-
